a4258cd48ae61e8dfecc87f9e672d02896e0c078
Glenn Penecios <163060428+gpenecios@users.noreply.github.com>
2026-01-21 14:43:36 -0600

    Update callteamswebhook.ps1

--- callteamswebhook.ps1
+++ callteamswebhook.ps1
@@ -1,13 +1,22 @@
 #Post in Teams General-Alerts Channel
 $script = "teamswebhook.ps1"
-$scriptdir = "C:\engrit\scripts\pshell"
+$scriptdir = "C:\scripts\glennp\sscripts"
+
 $webhook = "https://default44467e6f462c4ea2823f7800de5434.e3.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/bb3c6e365cd146acaae2927340be683d/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aw3h79I8xuD9x8Nja15deutMdW1aBkN03jJUr5Ku21E"
 
+# Server Info
+$ServerName = $env:computername
+$LogDir = "c:\scripts\glennp\logs"
+$LogFile = "$LogDir\$ServerName-update.log"
+$bodymsg = get-content -path "$LogFile"
+
+
 $curtime = Get-Date -Format "HH:mm:ss"
-$title = "Teams Notify"
-$severity = "HIGH"
-$msg = "This is a test message --> $severity --> $curtime"
-$log = "C:\engrit\logs\$script.log"
+$title = "Log: Azure Agent Update"
+$severity = "LOW"
+$msg = $bodymsg
+#$msg = "This is a test message --> $severity --> $curtime"
+$scriptlog = "$LogDir\$script.log"
 $scriptpath = "$scriptdir\$script"
 
 # Run the script
