d6c16d901c05ca5bb1107b286e855870fec35ebd
G <glennuiuc@gmail.com>
2026-01-30 12:26:54 -0600

    updated to include module versions

--- install-updates.ps1
+++ install-updates.ps1
@@ -1,11 +1,58 @@
-Import-Module PSWindowsUpdate
+# ============================
+#  LOAD HIGHEST VERSION OF PSWindowsUpdate
+# ============================
 
-#Install-Module -Name PSWindowsUpdate -Force; get-windowsupdate -verbose
+$AllVersions = Get-Module -ListAvailable PSWindowsUpdate |
+               Sort-Object Version -Descending
 
-$ServerName = $env:computername
-#$Cpath = (Get-Location).Path
-$Cpath = "c:\scripts\glennp\logs"
+$Selected = $AllVersions[0]   # pick highest version
+Import-Module $Selected -Force
+
+$Major = $Selected.Version.Major
+$Minor = $Selected.Version.Minor
+
+Write-Output "Loaded PSWindowsUpdate version: $Major.$Minor"
+
+
+# ============================
+#  LOGGING SETUP
+# ============================
+
+$ServerName = $env:COMPUTERNAME
+$Cpath = "C:\scripts\glennp\logs"
 $LogFile = "$Cpath\$ServerName-update.log"
 
-# Install update for AzureAgent,Malicious Software Removal Tool, MS Defender
-Get-WindowsUpdate -Verbose -install -IgnoreReboot -AcceptAll -KBArticleID 5077633,2267602,890830 | Out-file $LogFile -Encoding UTF8
+if (-not (Test-Path $Cpath)) {
+    New-Item -ItemType Directory -Path $Cpath | Out-Null
+}
+
+
+# ============================
+#  RUN UPDATE BASED ON VERSION
+# ============================
+
+if ($Major -eq 1) {
+    "Using Get-WUInstall (v1.x)" | Out-File $LogFile -Encoding UTF8
+
+    Get-WUInstall -Verbose -IgnoreReboot -AcceptAll `
+        -KBArticleID 5077633,2267602,890830 |
+        Out-File $LogFile -Append -Encoding UTF8
+}
+elseif ($Major -eq 2) {
+    "Using Install-WindowsUpdate (v2.x)" | Out-File $LogFile -Append -Encoding UTF8
+
+    Install-WindowsUpdate -Verbose -IgnoreReboot -AcceptAll `
+        -KBArticleID 5077633,2267602,890830 |
+        Out-File $LogFile -Append -Encoding UTF8
+}
+elseif ($Major -ge 3) {
+    "Using Get-WindowsUpdate (v3.x)" | Out-File $LogFile -Append -Encoding UTF8
+
+    Get-WindowsUpdate -Verbose -Install -IgnoreReboot -AcceptAll `
+        -KBArticleID 5077633,2267602,890830 |
+        Out-File $LogFile -Append -Encoding UTF8
+}
+else {
+    "ERROR: Unsupported PSWindowsUpdate version." |
+        Out-File $LogFile -Append -Encoding UTF8
+}
