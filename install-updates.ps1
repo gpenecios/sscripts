5064782422289fcf5bf8ddc6e0d6d978ec1eb02d
Glenn Penecios <163060428+gpenecios@users.noreply.github.com>
2026-02-24 10:42:22 -0600

    Update install-updates.ps1

--- install-updates.ps1
+++ install-updates.ps1
@@ -42,14 +42,14 @@ if ($Major -eq 1) {
 elseif ($Major -eq 2) {
     "Using Install-WindowsUpdate (v2.x)" | Out-File $LogFile -Encoding UTF8
 
-    Install-WindowsUpdate -Verbose -IgnoreReboot -AcceptAll `
+    Install-WindowsUpdate -Verbose -WindowsUpdate -IgnoreReboot -AcceptAll `
         -KBArticleID $KBArticles |
         Out-File $LogFile -Append -Encoding UTF8
 }
 elseif ($Major -ge 3) {
     "Using Get-WindowsUpdate (v3.x)" | Out-File $LogFile -Append -Encoding UTF8
 
-    Get-WindowsUpdate -Verbose -Install -IgnoreReboot -AcceptAll `
+    Get-WindowsUpdate -Verbose -WindowsUpdate -Install -IgnoreReboot -AcceptAll `
         -KBArticleID $KBArticles |
         Out-File $LogFile -Append -Encoding UTF8
 }
